2a14515eb7a2064c7e70edb6492308e518e3e3b2
anaobi <anaobii@gmail.com>
2024-11-08 14:35:27 +0000

    add build files

---
+++ recipe/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#$PYTHON -m pip install . -vv --no-deps --ignore-installed https://files.pythonhosted.org/packages/b8/5d/b4e7c6d67bc51d342104ca876bb22f4511094e06202f3dab1d6842340224/tableauhyperapi-0.0.20746-py3-none-win_amd64.whl
+
+#!/bin/bash
+$PYTHON -m pip install --no-deps --ignore-installed "${SRC_DIR}/tableauhyperapi-0.0.20746-py3-none-manylinux2014_x86_64.whl"
+
